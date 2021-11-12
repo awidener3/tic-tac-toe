66b6bf64d308dd3d07b50e471afc258dcafae710
Alex Widener <awidener3@gmail.com>
2021-11-12 12:03:18 -0500

    added ability to enter X's and O's, along with a reset button that clears array

---
+++ app.js
@@ -0,0 +1,89 @@
+// gameboard module
+
+const gameboard = (function() {
+    
+    const gameboardArea = document.getElementsByClassName('gameboard')[0];
+            
+    let board = [];
+    for (let i = 0; i < 9; i++) {
+        board.push('');
+        createSquare(board[i]);
+    }
+
+    function createSquare(item) {
+        let square = document.createElement('div');
+        square.innerText = item;
+        square.classList.add('gameboard-square');
+        gameboardArea.appendChild(square);
+    }
+
+    return {
+        board
+    }
+})();
+
+// displayController module
+
+const displayController = (function() {
+    const squares = document.getElementsByClassName('gameboard-square');
+    let { board } = gameboard; // grab board from gameboard module
+    const { createSquare } = gameboard;
+    const resetButton = document.getElementsByClassName('reset-btn')[0];
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', function() {
+            if (this.innerText === '') {
+                this.innerText = 'X';
+                board[i] = 'X';
+            } else if (this.innerText === 'X') {
+                this.innerText = 'O';
+                board[i] = 'O';
+            } else {
+                this.innerText = '';
+                board[i] = '';
+            }
+        })
+    }
+
+    resetButton.addEventListener('click', function(e) {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = '';
+            squares[i].innerText = '';
+        }
+    })
+
+    function checkWin() {
+
+        // horizontal wins
+        // win state 1 : [1: 'X', 2: 'X', 3: 'X' || 1: 'O', 2: 'O', 3: 'O']
+        // win state 2 : [4: 'X', 5: 'X', 6: 'X' || 4: 'O', 5: 'O', 6: 'O']
+        // win state 3 : [7: 'X', 8: 'X', 9: 'X' || 7: 'O', 8: 'O', 9: 'O']
+
+        // vertical wins
+        // win state 4 : [1: 'X', 4: 'X', 7: 'X' || 1: 'O', 4: 'O', 7: 'O']
+        // win state 5 : [2: 'X', 5: 'X', 8: 'X' || 2: 'O', 5: 'O', 8: 'O']
+        // win state 6 : [3: 'X', 6: 'X', 9: 'X' || 3: 'O', 6: 'O', 9: 'O']
+
+        // diagonal wins
+        // win state 7 : [1: 'X', 5: 'X', 9: 'X' || 1: 'O', 5: 'O', 9: 'O']
+        // win state 8 : [3: 'X', 5: 'X', 7: 'X' || 3: 'O', 5: 'O', 7: 'O']
+
+    }
+})();
+
+// Player factory
+
+const Player = () => {
+    
+    const add = () => {
+        console.log('hello');
+    }
+
+    return {
+        add
+    }
+}
+
+const player1 = Player();
+
+// player.add();
