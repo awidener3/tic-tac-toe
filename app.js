2d54acd35b7e3401b18cb7a0c8f90d258d43dc1a
Alex Widener <awidener3@gmail.com>
2021-11-15 11:11:27 -0500

    style changes

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
 // gameboard module
 
-const gameboard = (function() {
+// const gameboard = (function() {
     
-    const gameboardArea = document.getElementsByClassName('gameboard')[0];
+    // const gameboardArea = document.getElementsByClassName('gameboard')[0];
             
-    let board = [];
-    for (let i = 0; i < 9; i++) {
-        board.push('');
-        createSquare(board[i]);
-    }
+    // let board = [];
+    // for (let i = 0; i < 9; i++) {
+    //     board.push('');
+    //     createSquare(board[i]);
+    // }
 
-    function createSquare(item) {
-        let square = document.createElement('div');
-        square.innerText = item;
-        square.classList.add('gameboard-square');
-        gameboardArea.appendChild(square);
-    }
+    // function createSquare(item) {
+    //     let square = document.createElement('div');
+    //     square.innerText = item;
+    //     square.classList.add('gameboard-square');
+    //     gameboardArea.appendChild(square);
+    // }
 
-    return {
-        board
-    }
-})();
+    // return {
+    //     board
+    // }
+// })();
 
 // displayController module
 
-const displayController = (function() {
-    const squares = document.getElementsByClassName('gameboard-square');
-    let { board } = gameboard; // grab board from gameboard module
-    const { createSquare } = gameboard;
-    const resetButton = document.getElementsByClassName('reset-btn')[0];
+// const displayController = (function() {
+    // const squares = document.getElementsByClassName('gameboard-square');
+    // let { board } = gameboard; // grab board from gameboard module
+    // const { createSquare } = gameboard;
+    // const resetButton = document.getElementsByClassName('reset-btn')[0];
 
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', function() {
-            if (this.innerText === '') {
-                this.innerText = 'X';
-                board[i] = 'X';
-            } else if (this.innerText === 'X') {
-                this.innerText = 'O';
-                board[i] = 'O';
-            } else {
-                this.innerText = '';
-                board[i] = '';
-            }
-        })
-    }
+    // for (let i = 0; i < squares.length; i++) {
+    //     squares[i].addEventListener('click', function() {
+    //         if (this.innerText === '') {
+    //             this.innerText = 'X';
+    //             board[i] = 'X';
+    //         } else if (this.innerText === 'X') {
+    //             this.innerText = 'O';
+    //             board[i] = 'O';
+    //         } else {
+    //             this.innerText = '';
+    //             board[i] = '';
+    //         }
+    //     })
+    // }
 
-    resetButton.addEventListener('click', function(e) {
-        for (let i = 0; i < board.length; i++) {
-            board[i] = '';
-            squares[i].innerText = '';
-        }
-    })
+    // resetButton.addEventListener('click', function(e) {
+    //     for (let i = 0; i < board.length; i++) {
+    //         board[i] = '';
+    //         squares[i].innerText = '';
+    //     }
+    // })
 
-    function checkWin() {
+    // function checkWin() {
 
         // horizontal wins
         // win state 1 : [1: 'X', 2: 'X', 3: 'X' || 1: 'O', 2: 'O', 3: 'O']
@@ -68,22 +68,65 @@ const displayController = (function() {
         // win state 7 : [1: 'X', 5: 'X', 9: 'X' || 1: 'O', 5: 'O', 9: 'O']
         // win state 8 : [3: 'X', 5: 'X', 7: 'X' || 3: 'O', 5: 'O', 7: 'O']
 
-    }
-})();
+    // }
+// })();
 
-// Player factory
 
-const Player = () => {
-    
-    const add = () => {
-        console.log('hello');
-    }
 
-    return {
-        add
+
+// VARIABLE DECLARATION //
+const gameboardArea = document.getElementsByClassName('gameboard')[0];
+const resetButton = document.getElementsByClassName('reset-btn')[0];
+const squares = document.getElementsByClassName('gameboard-square');
+let board = [];
+
+function createGameBoard() {
+    for (let i = 0; i < 9; i++) {
+        board.push('');
+        createSquare();
     }
 }
 
-const player1 = Player();
+function createSquare() {
+    let square = document.createElement('div');
+    square.classList.add('gameboard-square');
+    gameboardArea.appendChild(square);
+}
 
-// player.add();
+let xMoves = 0; // this will eventually go into the player object
+let oMoves = 0; // ^^^
+
+function game () {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', function() {
+            if (squares[i].textContent == '') {
+                if (xMoves > oMoves) {
+                    squares[i].innerHTML = 'O'
+                    oMoves++;
+                    board[i] = 'O';
+                } else {
+                    squares[i].innerHTML = 'X'
+                    xMoves++;
+                    board[i] = 'X';
+                }
+            }
+            checkWin();
+        })
+    }
+}
+
+function checkWin () {
+}
+
+resetButton.addEventListener('click', function(e) {
+    for (let i = 0; i < board.length; i++) {
+        board[i] = '';
+        squares[i].innerText = '';
+    }
+    xMoves = 0;
+    oMoves = 0;
+})
+
+
+createGameBoard();
+game();
